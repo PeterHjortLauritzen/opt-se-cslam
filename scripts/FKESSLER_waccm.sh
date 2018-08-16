e36ab73a3e9e431e30085d0394528aca4b4ccec1
PeterHjortLauritzen <pel@ucar.edu>
2018-08-16 07:43:04 -0600

    mods

--- scripts/FKESSLER_waccm.sh
+++ scripts/FKESSLER_waccm.sh
@@ -22,6 +22,7 @@ setenv steps "5"
 #
 # DO NOT MODIFY BELOW THIS LINE
 #
+setenv nlev 70
 setenv cset "FKESSLER"
 setenv caze ${src}_${cset}_${res}_${pecount}_${steps}${stopoption}
 #
@@ -55,7 +56,7 @@ if ($res == "ne30pg3_ne30pg3_mg17") then
   ./xmlchange ROF2LND_FMAPNAME=$pg3map/map_0.5x0.5_nomask_TO_ne30pg3_aave_da_180515.nc
 endif
 ./xmlchange --append CAM_CONFIG_OPTS="-nadv_tt=100"
-./xmlchange CAM_CONFIG_OPTS="-phys kessler -chem terminator -analytic_ic -nadv_tt=$qsize"
+./xmlchange CAM_CONFIG_OPTS="-phys kessler -chem terminator -analytic_ic -nadv_tt=$qsize -nlev $nlev"
 ##
 ./xmlquery CAM_CONFIG_OPTS
 ./xmlquery EXEROOT
@@ -67,10 +68,27 @@ if ($res == "ne30pg3_ne30pg3_mg17") then
   echo "fsurdat='$pg3map/surfdata_ne30np4.pg3_78pfts_CMIP6_simyr2000_c180228.nc'">>user_nl_clm
 endif
 
+
+if ($cset == "FKESSLER") then
+cat >> /glade/scratch/$USER/$caze/SourceMods/src.cam/dctest_baro_kessler.xml <<EOF
+<?xml version="1.0"?>
+<namelist_defaults>
+<start_ymd> 10101 </start_ymd>
+<empty_htapes>.true.</empty_htapes>
+<avgflag_pertape>'I'</avgflag_pertape>
+<nhtfrq>-24</nhtfrq>
+<fincl1>
+ 'PS','PRECL'
+</fincl1>
+<fincl2>
+ 'Q','CLDLIQ','RAINQM','T','U','V','iCLy','iCL','iCL2','OMEGA'
+</fincl2>
+<analytic_ic_type>'baroclinic_wave'</analytic_ic_type>
+</namelist_defaults>
+EOF
+endif
+
 echo "se_statefreq       = 244"        >> user_nl_cam
-echo "empty_htapes       = .true."   >> user_nl_cam
-echo "fincl1             = 'PS','PSDRY','PSL','OMEGA','OMEGA500','OMEGA850','PRECL','PRECC',  "   >> user_nl_cam
-echo "                     'PTTEND','OMEGAT','CLDTOT','TMQ','T','U','V','Q'    " >> user_nl_cam
 echo "avgflag_pertape(1) = 'I'" >> user_nl_cam
 echo "nhtfrq             = -24,-24 " >> user_nl_cam
 echo "interpolate_output = .true.,.true." >> user_nl_cam
